d38f9f088c6909c9631b175beb2b3ba1686b5f88
nurullahChannext <88654682+nurullahemsen@users.noreply.github.com>
2023-09-26 10:26:12 +0200

    Update RabbitMQCommand.php

--- src/Console/Commands/RabbitMQCommand.php
+++ src/Console/Commands/RabbitMQCommand.php
@@ -35,7 +35,6 @@ class RabbitMQCommand extends Command
      * Execute the console command.
      *
      * @return mixed
-     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function handle()
     {
