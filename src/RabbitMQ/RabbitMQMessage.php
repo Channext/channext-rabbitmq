b9cdcc57eb42936b9994992e137872ff921044f6
RickFierens <81739745+RickFierens@users.noreply.github.com>
2023-12-14 11:10:48 +0100

    Added validator to rabbitmq message

--- src/RabbitMQ/RabbitMQMessage.php
+++ src/RabbitMQ/RabbitMQMessage.php
@@ -2,6 +2,9 @@
 
 namespace Channext\ChannextRabbitmq\RabbitMQ;
 
+use Channext\ChannextRabbitmq\Facades\RabbitMQ;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Message\AMQPMessage;
 
@@ -153,5 +156,22 @@ class RabbitMQMessage extends AMQPMessage
     public function getDeliveryInfo() : array
     {
         return $this->originalMessage->getDeliveryInfo();
+    }
+
+    /**
+     * @param $rules
+     * @return array
+     */
+    public function validate(array $rules, array $messages = []) : mixed
+    {
+        $validator = Validator::make($this->all(), $rules);
+        if($validator->fails()) {
+            $routingKey = $this->getRoutingKey();
+            $errors = $validator->errors()->all();
+            RabbitMQ::publish(body: $errors, routingKey: "$routingKey.failed");
+            return throw ValidationException::withMessages($errors);
+        }
+
+        return $validator->validated();
     }
 }
