1be4a37be8dd7e1c71cc142e26649efc5f91dfd4
nurullahChannext <88654682+nurullahemsen@users.noreply.github.com>
2025-02-05 11:48:39 +0100

    return unknown status if healthcheck cant reach rabbitmq broker

--- src/Http/Controllers/HealthcheckController.php
+++ src/Http/Controllers/HealthcheckController.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace Channext\ChannextRabbitmq\Http\Controllers;
 
-use Exception;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Http;
-
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use function Sentry\captureException;
 
 class HealthcheckController
@@ -24,7 +25,14 @@ class HealthcheckController
         try {
             $response = Http::withBasicAuth(config('rabbitmq.user'), config('rabbitmq.password'))
                 ->get($url);
-        } catch (Exception $e) {
+        }
+        catch (ConnectionException $e) {
+            captureException($e);
+            return response()->json([
+                'status' => 'unknown',
+                'message' => 'Message broker is down.'
+            ], 200);
+        } catch (\Throwable $e) {
             captureException($e);
             return response()->json(['status' => 'error'], 500);
         }
@@ -37,9 +45,8 @@ class HealthcheckController
             }
         }
         if ($consumerFound) {
-            return response()->json(['status' => 'ok']);
+            return response()->json(['status' => 'healthy'], 200);
         }
         return response()->json(['status' => 'error'], 500);
-
     }
 }
